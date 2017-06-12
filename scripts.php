4869c0a5a1c321645001625f7a8fcb921c937d13
Gleb Svechnikov <gleb.svechnikov@gmail.com>
2017-06-12 20:53:26 +0300

    Added VK Pixel code

--- scripts.php
+++ scripts.php
@@ -37,3 +37,6 @@
 	        } else { f(); }
 	})(document, window, "yandex_metrika_callbacks");
 </script>
+<script>
+	(window.Image ? (new Image()) : document.createElement('img')).src = 'https://vk.com/rtrg?p=VK-RTRG-54290-dfcGU';
+</script>
