ea685aa0ca586f6aea5782c221b87a2790b4b752
Gleb Svechnikov <gleb.svechnikov@gmail.com>
2017-08-04 19:18:23 +0300

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -19,7 +19,7 @@ function initMap() {
         lng: 30.3014
     }, {
         lat: 60.009,
-        lng: 30.254
+        lng: 30.248
     }];
 
     var map = new google.maps.Map(document.getElementById('map'), {
