e00d07e1edfd34a5d4d69ccfd45a90b1dd793946
Gleb Svechnikov <gleb.svechnikov@gmail.com>
2017-11-02 15:35:11 +0300

    revert last change

--- main/register.php
+++ main/register.php
@@ -21,8 +21,8 @@ if($_SERVER["REQUEST_METHOD"] === "POST") {
         unset($blocked[$_SERVER['REMOTE_ADDR']]);
     }
     if (!empty($_POST) &&
-        !(empty($_POST['name']) || empty($_POST['birthday']) || empty($_POST['phone']) || empty($_POST['email']) || empty($_POST['address'])) )
-//	&& !array_key_exists($_SERVER['REMOTE_ADDR'], $blocked)
+        !(empty($_POST['name']) || empty($_POST['birthday']) || empty($_POST['phone']) || empty($_POST['email']) || empty($_POST['address'])) 
+	&& !array_key_exists($_SERVER['REMOTE_ADDR'], $blocked)
     ) {
 
         $blocked[ $_SERVER['REMOTE_ADDR'] ] = time();
