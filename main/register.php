3c60d0f5c68540085e3cd7c4d50c097b0e4261e1
ksenyatru <ksenyatru@gmail.com>
2018-05-07 11:27:20 +0300

    Update register.php

--- main/register.php
+++ main/register.php
@@ -91,7 +91,7 @@ if($_SERVER["REQUEST_METHOD"] === "POST") {
 				<div class = "next">
 					Ближайшие занятия:
 					<div>
-						30 апреля - 6 мая
+						7 - 13 мая
 					</div>
 				</div>
 				<div class = "free">
