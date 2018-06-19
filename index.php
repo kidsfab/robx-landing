b8d5480615bbb62440ab9b26dce615230ade255f
Gleb Svechnikov <gleb.svechnikov@gmail.com>
2018-06-19 13:54:14 +0300

    Add mango chant script

--- index.php
+++ index.php
@@ -38,4 +38,5 @@
 	</body>
 
 	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDdON7rtEjNA84pctrH2T6yeR1OXyk0cpo&callback=initMap" async defer></script>
+	<script> (function(w, d, u, i, o, s, p) { if (d.getElementById(i)) { return; } w['MangoObject'] = o; w[o] = w[o] || function() { (w[o].q = w[o].q || []).push(arguments) }; w[o].u = u; w[o].t = 1 * new Date(); s = d.createElement('script'); s.async = 1; s.id = i; s.src = u; s.charset = 'utf-8'; p = d.getElementsByTagName('script')[0]; p.parentNode.insertBefore(s, p); }(window, document, '//widgets.mango-office.ru/widgets/mango.js', 'mango-js', 'mgo')); mgo({multichannel: {id: 1345}}); </script>
 </html>
