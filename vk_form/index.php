48b0da49e853094e486cd0268ecfc98cb18aa007
Gleb Svechnikov <glebsvechnikov@gleb.local>
2018-10-08 00:27:12 +0300

    Test post handling

--- vk_form/index.php
+++ vk_form/index.php
@@ -1,21 +1,7 @@
 
-<!DOCTYPE HTML>
-<html lang="en-US">
-<head>
-	<meta charset="UTF-8">
-	<title>VK lead</title>
-</head>
-<body style="background-color: #000000; color: #00FF00; font-family: monospace ; padding: 0 10px;">
-    Response
-<pre>
+
 <?php
-$content = trim(file_get_contents("php://input"));
-
-//Attempt to decode the incoming RAW post data from JSON.
-$decoded = json_decode($content, true);
-printf ($decoded);
-
+$data = json_decode(file_get_contents('php://input'), true);
+print_r($data);
+echo $data["group_id"];
 ?>
-</pre>
-</body>
-</html>
