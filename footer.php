0fd63bc97fe7dfe4ff3f7ed91c9982e70c3b66fd
Gleb Svechnikov <glebsvechnikov@gleb.local>
2018-10-07 23:37:02 +0300

    Update http > https urls

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer>
 	<div class = "vk-widget">
 		Проекты наших учеников, видео, фото с занятий в группе VK , подписывайтесь.
-		<script type="text/javascript" src="http://vk.com/js/api/openapi.js?136"></script>
+		<script type="text/javascript" src="https://vk.com/js/api/openapi.js?136"></script>
 		<div id="vk_groups"></div>
 		<script type="text/javascript">
 			VK.Widgets.Group("vk_groups", {mode: 3, width: Math.min(220, $('.vk-widget').width()) + ""}, 69501379);
