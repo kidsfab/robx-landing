9ca69bdce789ccf1a0c6e90f5f635f964dd78274
Gleb Svechnikov <glebsvechnikov@gleb.local>
2018-10-07 23:31:57 +0300

    Update http > https urls

--- main/video.php
+++ main/video.php
@@ -4,7 +4,7 @@
 					<h1>
 						Проекты наших учеников
 					</h1>
-					<div link = "http://www.youtube.com/embed/8tjduZiy1-w?rel=0&showinfo=0" >
+					<div link = "https://www.youtube.com/embed/8tjduZiy1-w?rel=0&showinfo=0" >
 						<h2>
 							Никита. Мультики
 						</h2>
@@ -12,7 +12,7 @@
 							Никита рассказывает о том, как на занятии ему удалось сделать свой мультик. Ребята написали специальную программу для LED-экрана.
 						</div>
 					</div>
-					<div link = "http://www.youtube.com/embed/VAWGhN5XJFA?rel=0&showinfo=0">
+					<div link = "https://www.youtube.com/embed/VAWGhN5XJFA?rel=0&showinfo=0">
 						<h2>
 							Стёпа. 3D-моделирование в OpenSCAD
 						</h2>
@@ -20,8 +20,8 @@
 							Стёпа создаёт объёмную фигурку в программе OpenSCAD. Подобные модельки дети печатают на нашем 3D-принтере.
 						</div>
 					</div>
-		
-					<div link = "http://www.youtube.com/embed/flspC5bX7eU?rel=0&showinfo=0">
+
+					<div link = "https://www.youtube.com/embed/flspC5bX7eU?rel=0&showinfo=0">
 						<h2>
 							Полина. Конденсатор
 						</h2>
@@ -29,7 +29,7 @@
 							Полина объясняет, что такое конденсатор. Она составила схему и программу, и показывает, как всё это работает.
 						</div>
 					</div>
-					<div link = "http://www.youtube.com/embed/WV2m-Lo-OsA?rel=0&showinfo=0">
+					<div link = "https://www.youtube.com/embed/WV2m-Lo-OsA?rel=0&showinfo=0">
 						<h2>
 							Катя. Электромагниты и магнитное поле
 						</h2>
@@ -37,12 +37,12 @@
 							Катя демонстрирует, как работает электромагнитная катушка. Затем у доски она объясняет принцип действия магнитного поля.
 						</div>
 					</div>
-					<div link = "http://www.youtube.com/embed/GLKrts26Sto?rel=0&showinfo=0">
+					<div link = "https://www.youtube.com/embed/GLKrts26Sto?rel=0&showinfo=0">
 						<h2>
 							Гоша. Электрическая цепь
 						</h2>
 						<div>
-							Гоша описывает путь электричества по электрической цепи. При помощи наглядной схемы он объясняет принцип работы гальванометра, 
+							Гоша описывает путь электричества по электрической цепи. При помощи наглядной схемы он объясняет принцип работы гальванометра,
 							конденсатора и других элементов цепи.
 						</div>
 					</div>
@@ -51,4 +51,3 @@
 					<iframe frameborder="0"></iframe>
 				</div>
 			</div>
-
