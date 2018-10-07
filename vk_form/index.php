529cf375f179abf6181f46889aae530faca45a43
Gleb Svechnikov <glebsvechnikov@gleb.local>
2018-10-08 00:03:39 +0300

    Use SDK for vk callback api

--- vk_form/index.php
+++ vk_form/index.php
@@ -23,3 +23,16 @@ $data = json_decode(file_get_contents('php://input'));
 $handler->parse($data);
 
 ?>
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>VK lead</title>
+</head>
+<body style="background-color: #000000; color: #00FF00; font-family: monospace ; padding: 0 10px;">
+    Request was
+<pre>
+<?php echo $_GET; ?>
+</pre>
+</body>
+</html>
