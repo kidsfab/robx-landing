c30eeed1bdc5162ae0396c932cc9f6eb26220fe2
Gleb Svechnikov <glebsvechnikov@gleb.local>
2018-10-08 00:20:36 +0300

    Test post handling

--- vk_form/index.php
+++ vk_form/index.php
@@ -8,7 +8,14 @@
 <body style="background-color: #000000; color: #00FF00; font-family: monospace ; padding: 0 10px;">
     Response
 <pre>
-<?php $_POST["group_id"]; ?>
+<?php
+printf ($_SERVER["REQUEST_METHOD")
+foreach ($_POST as $param_name => $param_val) {
+    printf "Param: $param_name; Value: $param_val<br />\n";
+}
+
+
+?>
 </pre>
 </body>
 </html>
