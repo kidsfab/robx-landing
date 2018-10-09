09ba9c919352c1e63e914558cc746826ee450d50
Gleb Svechnikov <glebsvechnikov@gleb.local>
2018-10-09 19:00:22 +0300

    Text VK callback api

--- vk_form/index.php
+++ vk_form/index.php
@@ -1,8 +1,27 @@
 
 
 <?php
-$rest_json = file_get_contents("php://input");
-$data = json_decode($rest_json, true);
+if (!isset($_REQUEST)) {
+    return;
+}
 
-print_r($data);
+//Строка для подтверждения адреса сервера из настроек Callback API
+$confirmation_token = 'e7e13fe2';
+
+//Получаем и декодируем уведомление
+$data = json_decode(file_get_contents('php://input'));
+
+//Проверяем, что находится в поле "type"
+switch ($data->type) {
+//Если это уведомление для подтверждения адреса...
+    case 'confirmation':
+        //...отправляем строку для подтверждения
+        echo $confirmation_token;
+        break;
+
+    default:
+        file_put_contents('result.txt', json_encode($data), FILE_APPEND);
+        break;
+
+}
 ?>
