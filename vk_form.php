1ddff5fccc657ebd43b2dcf655d402359bf335ce
Gleb Svechnikov <glebsvechnikov@gleb.local>
2018-10-10 13:04:31 +0300

    Move vk_form to root

--- vk_form.php
+++ vk_form.php
@@ -1,16 +1,11 @@
-
-
 <?php
 if (!isset($_REQUEST)) {
     return;
 }
-
 //Строка для подтверждения адреса сервера из настроек Callback API
 $confirmation_token = 'e7e13fe2';
-
 //Получаем и декодируем уведомление
 $data = json_decode(file_get_contents('php://input'));
-
 //Проверяем, что находится в поле "type"
 switch ($data->type) {
 //Если это уведомление для подтверждения адреса...
@@ -18,11 +13,9 @@ switch ($data->type) {
         //...отправляем строку для подтверждения
         echo $confirmation_token;
         break;
-
     default:
         file_put_contents('result.txt', json_encode($data), FILE_APPEND);
         echo 'ok';
         break;
-
 }
 ?>
