224cb1f92f6216073babd0f391c876a52db94131
Gleb Svechnikov <gleb.svechnikov@gmail.com>
2018-10-11 16:44:45 +0300

    Update vk_form.php

--- vk_form.php
+++ vk_form.php
@@ -28,12 +28,7 @@ switch ($data->type) {
             }
             if($first_name and $phone_number){
                 // отправляем данные.
-                senderToTallanto('Contact', array("first_name" => $first_name, 
-                                                  "phone_mobile" => $phone_number, 
-                                                  "utm_medium" => "social", 
-                                                  "utm_source"="VK"
-                                                 )
-                                );
+                senderToTallanto('Contact', array("first_name" => $first_name, "phone_mobile" => $phone_number, "utm_medium" => "social",  "utm_source" => "VK", "LBL_CONTACT_SOURCE" => "VK Tagrget"));
                 
                 echo 'ok';
                 break;
