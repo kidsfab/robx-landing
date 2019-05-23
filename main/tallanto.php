086f9f83a3c23be9ee507fcc878994de23c8b0a9
Gleb Svechnikov <gleb.svechnikov@simplinic.com>
2019-05-23 21:53:23 +0300

    Save response to file

--- main/tallanto.php
+++ main/tallanto.php
@@ -25,16 +25,13 @@ function senderToTallanto($module, $params)
 			'content' => http_build_query($params) ,
 		) ,
 	);
-	$file = 'test.txt';
-	// The new person to add to the file
-	// Write the contents to the file, 
-	// using the FILE_APPEND flag to append the content to the end of the file
-	// and the LOCK_EX flag to prevent anyone else writing to the file at the same time
-	file_put_contents($file, $params, FILE_APPEND | LOCK_EX);
+
 	try {
 		$context = stream_context_create($options);
 		$serverRes = file_get_contents($url, false, $context);
+		$file = 'res.txt';
 
+		file_put_contents($file, $serverRes, FILE_APPEND | LOCK_EX);
 		// Здесь можно произвести обработку запроса
 
 		$result = json_decode($serverRes, true);
