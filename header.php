1a29338811713afd4be6d767e17d3fbd7a3a9694
Gleb Svechnikov <gleb.svechnikov@simplinic.com>
2019-06-21 08:41:38 +0300

    Update advantages styles

--- header.php
+++ header.php
@@ -1,31 +1,28 @@
-		<header>
-			<div class = "logo">
-				<img src = "icons/logo.svg">
-			</div>
-
-			<a class = "burger"><img src = 'icons/burger.png'></a>
-			<a class = "cross"><img src = 'icons/cross.png'></a>
-
-			<div>
-				<a href = '#prices'>Стоимость</a>
-			</div>
-			
-			<div>
-				<a href = "#addresses">Контакты</a>
-			</div>
-			<div>
-				<a href = "https://edu.robx.org" style='text-align: center' target='_blank'>База знаний</a>
-			</div>
-			<div class="lk">
-				<a href="https://lk.robx.org"  target='_blank'>
-				<img src="icons/sign_in.svg">
-				<span>Вход</span>
-				</a>
-			</div>
-			<div class = "phone">
-				<a class="circled mgo-number-12398" href="tel:+78123177074">т. 317 70 74</a>
-			</div>
-			<div class = "register">
-				<a href = "#register" class = 'circled'>Записаться</a>
-			</div>
-		</header>
+<header>
+	<div class = "logo">
+		<img src = "icons/logo.svg">
+	</div>
+	<a class = "burger"><img src = 'icons/burger.png'></a>
+	<a class = "cross"><img src = 'icons/cross.png'></a>
+	<div>
+		<a href = '#prices'>Стоимость</a>
+	</div>
+	<div>
+		<a href = "#addresses">Контакты</a>
+	</div>
+	<div>
+		<a href = "https://edu.robx.org" style='text-align: center' target='_blank'>База знаний</a>
+	</div>
+	<div class="lk">
+		<a href="https://lk.robx.org"  target='_blank'>
+		<img src="icons/sign_in.svg">
+		<span>Вход</span>
+		</a>
+	</div>
+	<div class = "phone">
+		<a class="circled mgo-number-12398" href="tel:+78123177074">т. 317 70 74</a>
+	</div>
+	<div class = "register">
+		<a href = "#register" class = 'circled'>Записаться</a>
+	</div>
+</header>
