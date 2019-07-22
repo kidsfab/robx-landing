8155c4d1aa5607d28e5ee22e1ce89bc9303d4efd
Alexander Knop <aaknop@gmail.com>
2019-07-22 23:09:40 +0300

    Fix indentations

--- header.php
+++ header.php
@@ -1,27 +1,27 @@
-		<header>
-			<div class = "logo">
-				<img src = "icons/logo.svg">
-			</div>
+<header>
+	<div class = "logo">
+		<img src = "icons/logo.svg">
+	</div>
 
-			<a class = "burger"><img src = 'icons/burger.png'></a>
-			<a class = "cross"><img src = 'icons/cross.png'></a>
+	<a class = "burger"><img src = 'icons/burger.png'></a>
+	<a class = "cross"><img src = 'icons/cross.png'></a>
 
-			<div>
-				<a href = '#prices'>Стоимость</a>
-			</div>
-			<div>
-				<a href = "#addresses">Контакты</a>
-			</div>
-			<div>
-				<a href = "https://edu.robx.org" target='_blank'>База зананий</a>
-			</div>
-			<div>
-				<a href = "https://lk.robx.org" target='_blank'>Вход</a>
-			</div>
-            <div class = "register">
-				<a href = "#register" class = 'circled'>Записаться</a>
-			</div>
-			<div class = "phone">
-				<a class="circled mgo-number-12398" href="tel:+78123177074">т. 317 70 74</a>
-			</div>
-		</header>
+	<div>
+		<a href = '#prices'>Стоимость</a>
+	</div>
+	<div>
+		<a href = "#addresses">Контакты</a>
+	</div>
+	<div>
+		<a href = "https://edu.robx.org" target='_blank'>База зананий</a>
+	</div>
+	<div>
+		<a href = "https://lk.robx.org" target='_blank'>Вход</a>
+	</div>
+    <div class = "register">
+		<a href = "#register" class = 'circled'>Записаться</a>
+	</div>
+	<div class = "phone">
+		<a class="circled mgo-number-12398" href="tel:+78123177074">т. 317 70 74</a>
+	</div>
+</header>
