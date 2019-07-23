c4d338f92ca58306e308f778bc937cea161421f5
Alexander Knop <aaknop@gmail.com>
2019-07-23 11:42:08 +0300

    Update the review block

--- scripts/reviews.js
+++ scripts/reviews.js
@@ -4,44 +4,81 @@ function update_reviews() {
 function add_elements_computer_reviews() {
 	$('.reviews > ul > li:first-child').addClass('current');
 
-	// $('.reviews > ul').append('<a class = "left"></a>');
-	// $('.reviews > ul').append('<a class = "right"></a>');
+    var current = $('.reviews > ul > li:first-child');
+    for (var i = 0; i < 3; i++) {
+        current = current.next();
+    }
+    current.css("opacity", 0.5);
 
-	// if($('.reviews > ul > li.current~li').length < 2) {
-	// 	$('.reviews > ul a.right').hide();
-	// }
-    //
-	// if($('.reviews ul > li~li.current').length < 2) {
-	// 	$('.reviews > ul a.left').hide();
-	// }
+	$('.reviews > h1').append('<div>' +
+        '<a class = "left"></a><a class = "right"></a>' +
+        '</div>');
+
+	if($('.reviews ul > li.current').nextAll().length < 3) {
+        $('.reviews > h1 a.right').css("opacity", "0.5");
+	}
+
+	if($('.reviews ul > li.current').prevAll().length < 3) {
+		$('.reviews > h1 a.left').css("opacity", "0.5");
+	}
 
 
-	// $('.reviews > ul a.right').click(function() {
-	// 	$('.reviews > ul div.current').removeClass('current').next().next().addClass('current');
-	// 	$('.reviews > ul div').css('left', "-" + $('.reviews > ul > div.current').prevAll().length + "00%");
-    //
-	// 	if($('.reviews > ul > div.current').nextAll('div').length < 2) {
-	// 		$('.reviews > ul a.right').hide();
-	// 	}
-    //
-	// 	$('.reviews > ul a.left').show();
-	// });
+	$('.reviews > h1 a.right').click(function() {
+        if($('.reviews ul > li.current').nextAll().length >= 3) {
 
-	// $('.reviews > ul a.left').click(function() {
-	// 	$('.reviews > ul div.current').removeClass('current').prev().prev().addClass('current');
-	// 	$('.reviews > ul div').css('left', "-" + $('.reviews > ul > div.current').prevAll().length + "00%");
-    //
-	// 	if($('.reviews > ul > div.current').prevAll('div').length < 2) {
-	// 		$('.reviews > ul a.left').hide();
-	// 	}
-    //
-	// 	$('.reviews > ul a.right').show();
-	// });
+            $('.reviews > ul > li').css("opacity", 1);
 
+            var current = $('.reviews > ul > li.current');
+            for (var i = 0; i < 3; i++) {
+                current = current.next();
+            }
+            $('.reviews > ul > li.current').removeClass('current');
+    		current.addClass('current');
+            current.prev().css("opacity", 0.5);
+            for (var i = 0; i < 3; i++) {
+                current = current.next();
+            }
+            current.css("opacity", 0.5);
+    		$('.reviews > ul li').css('left', "-=102%");
+
+    		if($('.reviews ul > li.current').nextAll().length < 3) {
+    			$('.reviews > h1 a.right').css("opacity", "0.5");
+    		}
+
+    		$('.reviews > h1 a.left').css("opacity", "1");
+        }
+	});
+
+    $('.reviews > h1 a.left').click(function() {
+        if($('.reviews ul > li.current').prevAll().length >= 3) {
+
+            $('.reviews > ul > li').css("opacity", 1);
+
+            var current = $('.reviews > ul > li.current');
+            for (var i = 0; i < 3; i++) {
+                current = current.prev();
+            }
+            $('.reviews > ul > li.current').removeClass('current');
+    		current.addClass('current');
+            current.prev().css("opacity", 0.5);
+            for (var i = 0; i < 3; i++) {
+                current = current.next();
+            }
+            current.css("opacity", 0.5);
+    		$('.reviews > ul li').css('left', "+=102%");
+
+    		if($('.reviews ul > li.current').prevAll().length < 3) {
+    			$('.reviews > h1 a.left').css("opacity", "0.5");
+    		}
+
+    		$('.reviews > h1 a.right').css("opacity", "1");
+        }
+	});
 };
 
 
 function remove_elements_computer_reviews() {
+    $('.reviews > h1 > div').remove();
 }
 
 function add_elements_phone_reviews() {
@@ -59,6 +96,7 @@ function init_reviews() {
 	} else {
 		$('.reviews').get(0).mobile = false;
 		add_elements_computer_reviews();
+        $('.reviews ul, .reviews h1 div').css("visibility", "visible");
 	}
 }
 
