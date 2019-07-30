9d2fbe135c457ed52958c045570f88dbe6fe072e
Alexander Knop <aaknop@gmail.com>
2019-07-30 16:55:37 +0300

    Modify the menu

--- header.php
+++ header.php
@@ -6,20 +6,23 @@
 	<a class = "burger"><img src = 'icons/burger.png'></a>
 	<a class = "cross"><img src = 'icons/cross.png'></a>
 
+    <div class = "register">
+		<a href = "#register" class = 'circled'>Записаться</a>
+	</div>
+    <div>
+        <a href = "https://edu.robx.org">База знаний</a>
+    </div>
+    <div>
+        <a href = "https://edu.robx.org/parents/">Родителям</a>
+    </div>
 	<div>
-		<a href = '#prices'>Стоимость</a>
+		<a href = 'https://edu.robx.org/pay/'>Оплата</a>
 	</div>
 	<div>
-		<a href = "#addresses">Контакты</a>
-	</div>
-	<div>
-		<a href = "https://edu.robx.org">База знаний</a>
+		<a href = "https://edu.robx.org/contacts/">Контакты</a>
 	</div>
 	<div class = "login">
 		<a href = "https://lk.robx.org">Вход</a>
-	</div>
-    <div class = "register">
-		<a href = "#register" class = 'circled'>Записаться</a>
 	</div>
 	<div class = "phone">
 		<a class="circled mgo-number-12398" href="tel:+78123177074">т. 317 70 74</a>
