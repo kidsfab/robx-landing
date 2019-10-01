aeefec9bcaa886637138a05d24f9318ba11f72d9
strange9852 <strange9852@list.ru>
2019-10-01 16:58:52 +0300

    test source x2

--- main/register.php
+++ main/register.php
@@ -41,7 +41,7 @@ if($_SERVER["REQUEST_METHOD"] === "POST") {
 			'first_name' => $_POST['name'],
 			'phone_mobile' => $_POST['phone'],
 			'type_client_c' => 'ЛИД',
-      "source" => "school_site",
+      "source_contact_list" => "school_site",
       "utm_source" => "robx.org",
 		);
 
