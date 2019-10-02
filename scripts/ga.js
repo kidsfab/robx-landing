2c57dd95d8f1518ffc4eadd41effc71902267b8d
strange9852 <strange9852@list.ru>
2019-10-02 17:20:20 +0300

    google analitycs fix

--- scripts/ga.js
+++ scripts/ga.js
@@ -4,3 +4,4 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
 ga('create', 'UA-146792432-1', 'auto');
+ga('send', 'pageview');
